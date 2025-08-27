63fabfe9f747ff7d7d149a23728d27491117a9bf
Tony <twatson@linz.govt.nz>
2025-08-27 20:53:18 +1200

    Fix: Add sharp module configuration for Netlify builds

---
+++ netlify/plugins/prebuild-sharp/index.js
@@ -0,0 +1,13 @@
+// Netlify plugin to fix sharp installation
+module.exports = {
+  onPreBuild: async ({ utils }) => {
+    console.log('Installing sharp for linux-x64...');
+    try {
+      await utils.run.command('npm install --platform=linux --arch=x64 sharp');
+      console.log('Successfully installed sharp for linux-x64');
+    } catch (error) {
+      console.error('Failed to install sharp:', error);
+      // Don't fail the build - we'll let the normal build process try to handle it
+    }
+  }
+};
